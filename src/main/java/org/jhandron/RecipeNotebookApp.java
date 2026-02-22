4e4703cd1992e75247ff65d4384e75fcb234c329
Jason <jhandron@gmail.com>
2026-02-21 19:16:11 -0500

    Swing improvements

--- src/main/java/org/jhandron/RecipeNotebookApp.java
+++ src/main/java/org/jhandron/RecipeNotebookApp.java
@@ -2,13 +2,12 @@ package org.jhandron;
 
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import com.formdev.flatlaf.FlatLaf;
-import com.formdev.flatlaf.FlatLightLaf;
 import org.jhandron.ui.MainFrame;
 
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
 
 public final class RecipeNotebookApp {
-
 
     private RecipeNotebookApp() {
     }
@@ -32,8 +31,13 @@ public final class RecipeNotebookApp {
 
     private static void setupLookAndFeel() {
         try {
+            //Note: Invoke this method before setting the look and feel.
+            FlatLaf.registerCustomDefaultsSource("theme");
+//            UIManager.put("TabbedPane.showTabSeparators", true);
+//            UIManager.put("SplitPane.oneTouchButtonOffset", 5 );
             FlatIntelliJLaf.setup();
             FlatLaf.setUseNativeWindowDecorations(true);
+//            UIManager.put("TabbedPane.selectedBackground", Color.GRAY);
         } catch (Exception ex) {
             System.err.println("Failed to initialize LaF: " + ex.getMessage());
         }
